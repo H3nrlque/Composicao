8d330490a5799b97ba178c4d372163fa05eec168
Israel Henrique <israelhenrique854@gmail.com>
2024-05-19 21:21:55 -0300

    Por enquanto é como consigo fazer

--- src/application/PlaylistDeMusica.java
+++ src/application/PlaylistDeMusica.java
@@ -1,3 +1,70 @@
-public class PlaylistDeMusica {
+package application;
 
+import entities.Musicas;
+import entities.Playlist;
+import enums.StatusMusica;
+
+import java.util.Scanner;
+
+public class PlaylistDeMusica {
+    public static void main(String[] args) {
+        Scanner tc = new Scanner(System.in);
+        System.out.println("CRIANDO PLAYLIST");
+        System.out.print("Nome: ");
+        String nomePlaylist = tc.nextLine();
+        Playlist playlist = new Playlist(nomePlaylist);
+        System.out.println(StatusMusica.Playlist_Criada);
+        System.out.println();
+        int opcao;
+        do {
+            System.out.println(Escolha());
+            opcao = tc.nextInt();
+            if (opcao == 1) {
+                tc.nextLine();
+                System.out.print("Título: ");
+                String titulo = tc.nextLine();
+                System.out.print("Artista: ");
+                String artista = tc.nextLine();
+                System.out.print("Duração: ");
+                String duracao = tc.next();
+                Musicas musica = new Musicas(titulo, artista, duracao);
+                String verif = playlist.addMusicas(musica);
+                System.out.println(StatusMusica.valueOf(verif));
+                System.out.println();
+            } else if (opcao == 2) {
+                tc.nextLine();
+                System.out.print("Digite o título da musica que quer remover: ");
+                String titulo = tc.nextLine();
+                System.out.print("E o artista: ");
+                String artista = tc.nextLine();
+                String verif = playlist.removeMusicas(titulo, artista);
+                System.out.println(StatusMusica.valueOf(verif));
+                System.out.println();
+            } else if (opcao == 3) {
+                tc.nextLine();
+                System.out.print("Digite o título da música: ");
+                String titulo = tc.nextLine();
+                System.out.print("E o artista: ");
+                String artista = tc.nextLine();
+                String verif = playlist.PesquisarMusica(titulo, artista);
+                System.out.println(StatusMusica.valueOf(verif));
+                System.out.println();
+            } else if (opcao == 4) {
+                System.out.println(playlist);
+            } else if (opcao == 5) {
+                System.out.println("Saindo...");
+            }
+        } while (opcao != 5);
+    }
+
+    public static String Escolha() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Escolha uma opção: \n");
+        sb.append("[1] Adicionar Música a playlist\n");
+        sb.append("[2] Remover música da playlist\n");
+        sb.append("[3] Procurar música na playlist\n");
+        sb.append("[4] Ver playlist\n");
+        sb.append("[5] Sair\n");
+        return sb.toString();
+    }
 }
--- src/entities/Playlist.java
+++ src/entities/Playlist.java
@@ -1,2 +1,95 @@
-package entities;public class Playlist {
-}
+package entities;
+
+import enums.StatusMusica;
+
+import java.util.ArrayList;
+import java.util.List;
+
+    public class Playlist {
+        private String nomePlaylist;
+
+        private StatusMusica statusMusica;
+
+        private List<Musicas> musicas = new ArrayList<>();
+
+        public Playlist(String nomePlaylist) {
+            this.nomePlaylist = nomePlaylist;
+        }
+
+        public String getNomePlaylist() {
+            return nomePlaylist;
+        }
+
+        public void setNomePlaylist(String nomePlaylist) {
+            this.nomePlaylist = nomePlaylist;
+        }
+
+        public StatusMusica getStatusMusica() {
+            return statusMusica;
+        }
+
+        public void setStatus(StatusMusica statusMusica) {
+            this.statusMusica = statusMusica;
+        }
+
+        public List<Musicas> getMusicas() {
+            return musicas;
+        }
+
+        public String addMusicas(Musicas musica) {
+            String verif = null;
+            if (musicas.isEmpty()) {
+                musicas.add(musica);
+                verif = ("Musica_Adicionada");
+
+            } else  {
+                for (int i = 0; i < musicas.size(); i++) {
+                    if (musica.getTitulo().equals(musicas.get(i).getTitulo()) && musica.getArtista().equals(musicas.get(i).getArtista())) {
+                        verif = ("Musica_Ja_Adicionada_Anteriormente");
+                    } else {
+                        musicas.add(musica);
+                        verif = ("Musica_Adicionada");
+                    }
+                }
+            }
+                return verif;
+        }
+
+        public String removeMusicas(String titulo, String artista) {
+            String verif = null;
+            for (int i = 0; i < musicas.size(); i++) {
+                if (musicas.get(i).getTitulo().equals(titulo)) {
+                    musicas.remove(musicas.get(i));
+                    verif = ("Musica_Removida");
+                } else {
+                    verif = ("Musica_Nao_Encontrada");
+                }
+            }
+            return verif;
+        }
+
+        public String PesquisarMusica(String titulo, String artista) {
+            String verif = null;
+            for (int i = 0; i < musicas.size(); i++) {
+                if (musicas.get(i).getTitulo().equals(titulo) && musicas.get(i).getArtista().equals(artista)) {
+                    verif = "Música prsente nesta playlist";
+                } else {
+                    verif = ("Música não encontrada");
+                }
+            }
+            return verif;
+        }
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            sb.append("Playlist: ");
+            sb.append(nomePlaylist+"\n");
+            sb.append("Número de músicas: ");
+            sb.append(musicas.size()+"\n");
+            for (Musicas m : musicas) {
+                sb.append(m+"\n");
+            }
+            return sb.toString();
+
+        }
+    }
